a563bf4f9cd9a0b4404335f7647d4e5055a23fe5
Julian Stecklina <js@alien8.de>
2020-01-15 00:00:25 +0100

    Move boot image generation into a subcommand

--- app/Main.hs
+++ app/Main.hs
@@ -8,13 +8,13 @@ import           ApplicationDescription
 import           EpoxyMain
 import           MachineDescription
 
-data Arguments = Arguments
+data BootImageArguments = BootImageArguments
   { machineJsonFile     :: FilePath,
     applicationJsonFile :: FilePath,
     kernelTemplateFile  :: FilePath,
     outputFile          :: FilePath }
 
-doEpoxy :: Arguments -> IO ()
+doEpoxy :: BootImageArguments -> IO ()
 doEpoxy args = do
   elf <- parseElfFile (kernelTemplateFile args)
   machineDesc <- parseMachineDescription (machineJsonFile args)
@@ -23,13 +23,17 @@ doEpoxy args = do
   B.writeFile (outputFile args) (generateBootImage machineDesc elf processes)
   putStrLn "Done!"
 
-arguments :: Parser Arguments
-arguments = Arguments
+bootImageParser :: Parser BootImageArguments
+bootImageParser = BootImageArguments
   <$> strOption (long "machine" <> metavar "MACHINE" <> help "The machine description JSON file")
   <*> strOption (long "application" <> metavar "APPLICATION" <> help "The application description JSON file")
   <*> strOption (long "kernel" <> metavar "KERNEL" <> help "The kernel ELF file")
   <*> strOption (long "output" <> short 'o' <> metavar "OUTPUT" <> help "The bootable output ELF file")
 
+cmdParser :: Parser BootImageArguments
+cmdParser = subparser
+  (command "boot-image" (info (bootImageParser <**> helper) (progDesc "Build a bootable Epoxy system image")))
+
 main :: IO ()
 main = doEpoxy =<< execParser opts
-  where opts = info (arguments <**> helper) (fullDesc <> progDesc "Build a bootable Epoxy system image")
+  where opts = info (cmdParser <**> helper) (fullDesc <> progDesc "The Epoxy Swiss Army Knife")
