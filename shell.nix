eed4cf4b6d0a553a5f31ce922429a7b48f55750d
Julian Stecklina <js@alien8.de>
2020-04-09 00:54:21 +0200

    Fix shell.nix

--- shell.nix
+++ shell.nix
@@ -1,14 +1,17 @@
 { sources ? import ./nix/sources.nix
-, nixpkgs ? sources.nixpkgs
-, pkgs ? import nixpkgs (import sources.haskell-nix)
+, haskell-nix ? import sources.haskell-nix {}
+, nixpkgs ? haskell-nix.sources.nixpkgs-default
+, pkgs ? import nixpkgs haskell-nix.nixpkgsArgs
 , lib ? pkgs.lib }:
 pkgs.mkShell {
   buildInputs = [
-    pkgs.niv
+    # This can be replaced with pkgs.niv once nixpkgs is new enough.
+    (import sources.niv {}).niv
+
     pkgs.stack
     pkgs.haskell-nix.nix-tools
     # Broken :(
-    # pkgs.stylish-haskell
+     pkgs.stylish-haskell
     pkgs.hlint
   ];
 }
