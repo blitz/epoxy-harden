680bc128599ed8607cf8e16394486c2bbe3b0638
Julian Stecklina <js@alien8.de>
2020-04-09 01:09:15 +0200

    Update to Stackage LTS 15.7

--- pkgs.nix
+++ pkgs.nix
@@ -1,13 +1,6 @@
 {
   extras = hackage:
-    {
-      packages = {
-        "elf" = (((hackage.elf)."0.30").revisions).default;
-        "dhall" = (((hackage.dhall)."1.30.0").revisions).default;
-        "prettyprinter" = (((hackage.prettyprinter)."1.5.1").revisions).default;
-        epoxy-harden = ./epoxy-harden.nix;
-        };
-      };
-  resolver = "lts-14.27";
+    { packages = { epoxy-harden = ./epoxy-harden.nix; }; };
+  resolver = "lts-15.7";
   modules = [ ({ lib, ... }: { packages = {}; }) { packages = {}; } ];
   }
