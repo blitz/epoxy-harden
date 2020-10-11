074c1fd764e548cf2a13e0fccd68a2b7a2eb0ff1
Julian Stecklina <js@alien8.de>
2020-10-11 22:14:35 +0200

    Enable unit tests in CI

--- nix/ci.nix
+++ nix/ci.nix
@@ -1,5 +1,18 @@
-let release = import ./release.nix { };
+{ sources ? import ./sources.nix, haskell-nix ? import sources.haskell-nix { }
+, nixpkgs ? haskell-nix.sources.nixpkgs
+, pkgs ? import nixpkgs haskell-nix.nixpkgsArgs }:
+let
+  release = import ./release.nix { inherit sources haskell-nix nixpkgs pkgs; };
+  epoxy-harden = release.epoxyHarden.epoxy-harden;
 in {
-  epoxy-harden = release.epoxyHarden.epoxy-harden.components.exes.epoxy-harden;
+  epoxy-harden = epoxy-harden.components.exes.epoxy-harden;
+
+  # I'm too stupid to use the built-in support for running unit tests
+  # in haskell.nix, so let's do it the manual way.
+  tests = let testDrv = epoxy-harden.components.tests.epoxy-harden-test;
+  in pkgs.runCommand "epoxy-harden-test" { } ''
+    ${testDrv}/bin/epoxy-harden-test | tee $out
+  '';
+
   dhall = release.epoxyHarden.dhall.components.exes.dhall;
 }
