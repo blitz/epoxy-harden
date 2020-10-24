fea1cb4a82d32f555b53eb03b67c86a19d9dd37e
Julian Stecklina <js@alien8.de>
2020-10-24 17:06:34 +0300

    Don't blindly re-export RISC-V page table functions

--- src/PageTable.hs
+++ src/PageTable.hs
@@ -1,7 +1,28 @@
 module PageTable (constructPageTable, realizePageTables) where
 
-import           RiscV.PageTable
+import           Control.Monad.State.Lazy
 
--- This module only re-exports the RiscV page table functions, but we
--- need to implement support for different formats here. We also need
--- to separate the deduplication from the actual page table creation.
+import           AddressSpace
+import           EpoxyState
+import qualified RiscV.PageTable as R5
+import           FrameAlloc
+
+-- TODO We need to implement support for different formats here. We
+-- also need to separate the deduplication from the actual page table
+-- creation.
+
+-- | Create page table structures for an address space.
+--
+-- All page tables that are created have to be realized with
+-- 'realizePageTables' later.
+constructPageTable :: AddressSpace -> R5.PageTable
+constructPageTable = R5.constructPageTable
+
+-- |Realize a set of page tables by allocating physical memory for them.
+--
+-- An instance of 'PageTable' does not have backing store allocated
+-- for itself. This happens in the realization stage in this
+-- function. 'realizePageTables' takes a list of page tables to
+-- exploit opportunities to share page table structures.
+realizePageTables :: [R5.PageTable] -> State Epoxy [Frame]
+realizePageTables = R5.realizePageTables
