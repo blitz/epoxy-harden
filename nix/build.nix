ed6ce5d63ae89da967fab186a2d5b9894a97b077
Julian Stecklina <js@alien8.de>
2020-12-29 12:42:26 +0100

    Fix stack index to speed up evaluation

--- nix/build.nix
+++ nix/build.nix
@@ -6,4 +6,7 @@ haskell-nix.stackProject {
     name = "epoxy-harden";
     src = ./..;
   };
+
+  index-state = "2020-12-29T00:00:00Z";
+  stack-sha256 = "1b65r1k5js5zgiwq1g2y59b506zlczm1p6vlhx5hpq5qlqqs4pzk";
 }
